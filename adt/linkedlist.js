5f06875ea00edcdbcbeda954fd428af6739008f1
Danny Robinson <daniel@djrobinson.me>
2016-05-17 15:27:26 -0600

    Add tail exercise

--- adt/linkedlist.js
+++ adt/linkedlist.js
@@ -6,9 +6,24 @@ var LinkedList = function(node){
 
 LinkedList.prototype.newHead = function(data){
   var node = new Node(data);
-  console.log(this.head, node);
-  node.next = this.head;
-  this.head = node;
+  if (!this.head) {this.head = node;}
+  else{
+    node.next = this.head;
+    this.head = node;
+  }
+  return this;
+};
+
+LinkedList.prototype.insertTail = function(data){
+  var node = new Node(data);
+  if (!this.head) {this.head = node;}
+  else {
+    var temp = this.head;
+    while ( temp.next ){
+      temp = temp.next;
+    }
+    temp.next = node;
+  }
 };
 
 module.exports = LinkedList;
