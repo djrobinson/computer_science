47ebe08540ffa7ccdd535b539aaba5f295a89043
Danny Robinson <daniel@djrobinson.me>
2016-05-17 16:51:55 -0600

    Final Linked Lists prev, next, tail, head

--- adt/linkedlist.js
+++ adt/linkedlist.js
@@ -2,13 +2,17 @@ var Node = require('./node');
 
 var LinkedList = function(node){
   this.head = node;
+  this.tail = node;
 };
 
-LinkedList.prototype.newHead = function(data){
+LinkedList.prototype.insertHead = function(data){
   var node = new Node(data);
-  if (!this.head) {this.head = node;}
-  else{
+  if (!this.head) {
+    this.head = node;
+    this.tail = this.head;
+  } else {
     node.next = this.head;
+    this.head.prev = node;
     this.head = node;
   }
   return this;
@@ -16,14 +20,20 @@ LinkedList.prototype.newHead = function(data){
 
 LinkedList.prototype.insertTail = function(data){
   var node = new Node(data);
-  if (!this.head) {this.head = node;}
-  else {
+  if (!this.head) {
+    this.head = node;
+    this.tail = this.head;
+  } else {
     var temp = this.head;
     while ( temp.next ){
+      temp.next.prev = temp;
       temp = temp.next;
     }
     temp.next = node;
+    this.tail = node;
+    temp.next.prev = temp;
   }
+  return this;
 };
 
 module.exports = LinkedList;
